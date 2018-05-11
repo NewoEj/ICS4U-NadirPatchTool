73a6fcc17cb510015785200a2dd3f9bf62aaed9e
NewoEj <newoej@gmail.com>
2018-05-11 09:52:01 -0400

    Added working program
    This program works, except that it doesn't save the final picture in the correct location.

--- NadirPatchTool.py
+++ NadirPatchTool.py
@@ -1,21 +1,35 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
 from PIL import Image
 
 root = Tk()
 root.title("Nadir Patch Tool")
 
 def choose_photosphere():
-    global photosphere 
-    photosphere = filedialog.askopenfilename(filetypes=(("jpeg files","*.jpg"),("all files","*.*")))
+    global photosphere_path
+    photosphere_path = filedialog.askopenfilename()
 
 def choose_nadir():
-    global nadir
-    nadir = filedialog.askopenfilename(filetypes=(("jpeg files","*.jpg"),("all files","*.*")))
+    global nadir_path
+    nadir_path = filedialog.askopenfilename()
 
 def choose_save_destination():
+    global save_destination 
     save_destination = filedialog.askdirectory()
+
+def resize_nadir():
+    global nadir 
+    nadir = Image.open(nadir_path)
+    global photosphere
+    photosphere = Image.open(photosphere_path)
+    nadir = nadir.resize(photosphere.size, resample=0)
+    
+def patch():
+    photosphere.paste(nadir,(0,0),nadir)
+    photosphere.save("patchedsphere.jpeg","JPEG")
+    messagebox.showinfo(title="Program complete.", message="The photosphere has been patched and saved.")
 
 mainframe = ttk.Frame(root, padding="10")
 mainframe.columnconfigure(0, weight=1)
@@ -23,14 +37,18 @@ mainframe.rowconfigure(0, weight=1)
 one = ttk.Button(mainframe, text='Choose the photosphere to be patched.', command=choose_photosphere)
 two = ttk.Button(mainframe, text='Choose the nadir to be used.', command=choose_nadir)
 three = ttk.Button(mainframe, text='Choose the save location of the patched photosphere.', command=choose_save_destination)
-four = ttk.Button(mainframe, text='Quit program.', command=exit)
-five = ttk.Label(mainframe, text='Nadir Patch Tool', font=("Arial", 20))
+four = ttk.Button(mainframe, text='Resize nadir.', command=resize_nadir)
+five = ttk.Button(mainframe, text='Quit.', command=exit)
+six = ttk.Button(mainframe, text='Patch photosphere.', command=patch)
+seven = ttk.Label(mainframe, text='Nadir Patch Tool', font=("Arial", 20))
 
 mainframe.grid(column=0, row=0)
 one.grid(column=0, row=1, padx=10, pady=10)
 two.grid(column=0, row=2, padx=10, pady=10)
 three.grid(column=0, row=3, padx=10, pady=10)
 four.grid(column=0, row=4, padx=10, pady=10)
-five.grid(column=0, row=0)
+five.grid(column=0, row=6, padx=10, pady=10)
+six.grid(column=0, row=5, padx=10, pady=10)
+seven.grid(column=0, row=0)
 
 root.mainloop()
